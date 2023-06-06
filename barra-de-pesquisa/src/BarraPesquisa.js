32eb04f1cf41c16f7c742d9312b6b0f0e785fc19
weslleyf14 <weslleyf14@gmail.com>
2023-06-06 13:31:15 -0300

    quase funcionando

--- barra-de-pesquisa/src/BarraPesquisa.js
+++ barra-de-pesquisa/src/BarraPesquisa.js
@@ -1,5 +1,12 @@
 import React, { useState } from 'react';
 import './BarraPesquisa.css';
+<<<<<<< Updated upstream
+=======
+import axios from 'axios';
+import './App.css';
+import Header from './components/Header.js';
+class BarraPesquisa extends React.Component{
+>>>>>>> Stashed changes
 
 const BarraPesquisa = ({ onSearch }) => {
   const [searchTerm, setSearchTerm] = useState('');
@@ -8,6 +15,7 @@ const BarraPesquisa = ({ onSearch }) => {
     setSearchTerm(event.target.value);
   };
 
+<<<<<<< Updated upstream
   const handleSubmit = (event) => {
     event.preventDefault();
     onSearch(searchTerm);
@@ -24,6 +32,61 @@ const BarraPesquisa = ({ onSearch }) => {
       <button className='barra-pesquisa button' type="submit">Buscar</button>
     </form>
   );
+=======
+  
+  handleSubmit = async(event) => {
+    event.preventDefault();
+    try{
+      const url = `https://api.musixmatch.com/ws/1.1/track.search?q=${this.state.pesquisa}&apikey=bd42c6400b7e16099ab400e78fbb08b0`
+      const response = await fetch(url)
+      const json = await response.json()
+
+
+    }catch(err){
+      alert(err)
+    }
+
+  }
+
+  searchMusixmatch = async () => {
+    axios.get(`https://api.musixmatch.com/ws/1.1/track.search?q=${this.state.pesquisa}&apikey=bd42c6400b7e16099ab400e78fbb08b0`)
+    .then(res => {
+        const headerDate = res.headers && res.headers.date ? res.headers.date : 'no response date';
+        console.log('Status Code:', res.status);
+        console.log('Date in Response header:', headerDate);
+      
+        const users = res.data;
+      
+      })
+      .catch(err => {
+        console.log('Error: ', err.message);
+      })
+    }
+
+  
+  render(){
+    return (
+    <div>
+      <Header />
+      <div className='barra-container'>
+        <h1>O que você gostaria de cantar?</h1>
+        <div className='barra-pesquisa'>          
+          <form onSubmit={this.handleSubmit}>
+            <input className={this.state.pesquisa !== "" ? "has-val input" : "input"}
+              type="text"
+              id="pesquisa"
+              name="pesquisa"
+              value={this.state.pesquisa}
+              onChange={this.handleChange}
+            />
+            <button   type="submit" value="Submit">Buscar</button>
+          </form>
+        </div>
+      </div>
+    </div>
+    );
+  }
+>>>>>>> Stashed changes
 };
 
 export default BarraPesquisa;
--- karaoke/src/App.js
+++ karaoke/src/App.js
@@ -6,24 +6,26 @@ import NomeMusica from "./nome-musica";
 import { lyricsAtom } from "./store";
 import MusicaRateInput from "./musica-rate-input";
 import MensagemErro from "./mensagem-erro";
+//import BarraPesquisa from "./singstar/barra-de-pesquisa/src/BarraPesquisa";
+import Bora from "./bora";
 
 export default function App() {
     const lyrics = useAtomValue(lyricsAtom);
 
     const renderedChild =
         lyrics === "" ? (
-            <microphoneInput />
+            <MicrophoneInput />
         ) : (
             <>
                 <NomeMusica />
                 <CorpoLetra />
                 <MusicaRateInput />
             </>
-        );
+        )
 
     return (
         <main className="w-full h-full px-10 flex flex-col justify-center items-center">
-            <h1 className="font-bold font-sans text-6xl">KaraokeNow</h1>
+            <h1 className="font-bold font-sans text-6xl">Singstar</h1>
             <p className="mb-5">React ⚛️ + Vite ⚡ + Replit 🌀</p>
             {renderedChild}
             <MensagemErro />
